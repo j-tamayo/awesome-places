e5032c9541f4bff099fc04b6281b7a43adab901e
junior.pena <jtamayodev@gmail.com>
2017-06-20 16:17:51 -0400

    Adding maps

--- src/pages/add-place/add-place.ts
+++ src/pages/add-place/add-place.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams,ModalController } from 'ionic-angular';
 import { NgForm } from '@angular/forms';
+
+import { SetLocationPage } from '../set-location/set-location';
 
 @IonicPage()
 @Component({
@@ -9,11 +11,21 @@ import { NgForm } from '@angular/forms';
 })
 export class AddPlacePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private modalCtrl: ModalController
+  ) {
   }
 
   onSubmit(form: NgForm) {
     console.log(form.value);
+  }
+
+  onOpenMap() {
+    console.log('MAPPP');
+    const modal = this.modalCtrl.create(SetLocationPage);
+    modal.present();
   }
 
 }
